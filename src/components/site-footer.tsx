07d06a2735d99f9e0db3c94412a3472cd881e110
Lucas Svens <svenslucas@gmail.com>
2026-03-05 19:42:23 +0200

    added own branding in footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,11 +1,26 @@
-﻿export default function SiteFooter() {
+"use client";
+
+import { usePathname } from "next/navigation";
+
+export default function SiteFooter() {
+  const pathname = usePathname() ?? "/";
+  const isFinnish = pathname.startsWith("/fi");
+
   return (
     <footer className="site-footer">
       <div className="container site-footer-inner">
-        <p>Convoy Customs</p>
-        <p>Vasavägen 837, 66710 Kaitsor</p>
-        <p>0456625484 | info@convoycustoms.fi</p>
+        <div className="site-footer-left">
+          <p>Convoy Customs</p>
+          <p>Vasavägen 837, 66710 Kaitsor</p>
+          <p>0456625484 | info@convoycustoms.fi</p>
+        </div>
+        <div className="site-footer-right">
+          <p className="site-footer-credit">
+            {isFinnish ? "Luonut Lumix Systems" : "Skapat av Lumix Systems"}
+          </p>
+        </div>
       </div>
     </footer>
   );
 }
+
